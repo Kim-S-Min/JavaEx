155ce7c4af234bfc16ea1897726a39f6afe47d2d
김상민 <wrt123123123@gmail.com>
2021-02-25 14:47:04 +0900

    Initial Commit

---
+++ JavaEx/src/com/javaex/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.javaex;
+
+//	주석 : 컴파일되지 않는 설명문
+public class HelloWorld {
+	/*
+	 * 여러 줄 작성할수 있는 주석
+	 */
+	// 객채가 실행하고자 한다면 main 메서드가 필요
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");	//실행문 뒤에는 세미클론(;)을 꼭 넣어주자
+	}
+}
