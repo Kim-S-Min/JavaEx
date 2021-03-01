d98a61eb9134b1009eb8ec3f27d9582fb84b773a
김상민 <wrt123123123@gmail.com>
2021-03-01 19:25:19 +0900

    git

--- JavaEx/src/com/javaex/test/GitTest.java
+++
@@ -1,8 +0,0 @@
-package com.javaex.test;
-
-public class GitTest {
-	public static void main(String[] args) {
-		System.out.println("GitHubTest/ hello, github");
-	}
-}
-//	이 내용은 github test를 하기 위한 내용이다.
--- JavaEx/src/com/test/GitHubTest.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class GitHubTest {
-
-	public static void main(String[] args) {
-		System.out.println("hi");
-	}
-
-}
