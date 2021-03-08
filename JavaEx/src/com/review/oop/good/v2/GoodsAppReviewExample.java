05f72d932623a8da17e0261a1fcc51024af89b8e
김상민 <wrt123123123@gmail.com>
2021-03-09 08:52:47 +0900

    review

--- JavaEx/src/com/review/oop/good/v2/GoodsAppReviewExample.java
+++ JavaEx/src/com/review/oop/good/v2/GoodsAppReviewExample.java
@@ -1,23 +1,23 @@
 package com.review.oop.good.v2;
 
 public class GoodsAppReviewExample {
-	public static void main(String[] args) {
-		GoodsReviewExample notebook = new GoodsReviewExample();
+//	public static void main(String[] args) {
+//		GoodsReviewExample notebook = new GoodsReviewExample();
 		//	setter를 이용한 우회 접근
-		notebook.setName("LG Gram");
-		notebook.setPrice(1500000);
+//		notebook.setName("LG Gram");
+//		notebook.setPrice(1500000);
 		
-		GoodsReviewExample smartphone = new GoodsReviewExample();
-		smartphone.setName("iPhone SE");
-		smartphone.setPrice(650000);
+//		GoodsReviewExample smartphone = new GoodsReviewExample();
+//		smartphone.setName("iPhone SE");
+//		smartphone.setPrice(650000);
 		
-		System.out.printf("%s, %d원%n",
-				notebook.getName(), notebook.getPrice());
-		notebook.showInfo();	//	get을 set안에 집어 넣어서 set으로 private를 우회해서 get값을 출력한다
+//		System.out.printf("%s, %d원%n",
+//				notebook.getName(), notebook.getPrice());
+//		notebook.showInfo();	//	get을 set안에 집어 넣어서 set으로 private를 우회해서 get값을 출력한다
 		
 		
-		System.out.printf("%s, %d원%n",
-				smartphone.getName(), smartphone.getPrice());
-		smartphone.showInfo();
-	}
+//		System.out.printf("%s, %d원%n",
+//				smartphone.getName(), smartphone.getPrice());
+//		smartphone.showInfo();
+//	}
 }
