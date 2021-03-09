107a0d186acab6ed3da51682f101e1b0c63430d7
김상민 <wrt123123123@gmail.com>
2021-03-09 11:47:21 +0900

    Hashtable

---
+++ JavaEx/src/com/javaex/collection/hash/ClassRoom.java
@@ -0,0 +1,20 @@
+package com.javaex.collection.hash;
+
+
+public class ClassRoom {
+	private String subject;
+	private String roomName;
+	
+	public ClassRoom(String subject, String roomName) {
+		this.subject = subject;
+		this.roomName = roomName;
+		
+		
+	}
+	@Override
+	public String toString() {
+		return "ClassRoom [subject=" + subject + ", roomName=" + roomName + "]";
+		
+		
+	}
+}
