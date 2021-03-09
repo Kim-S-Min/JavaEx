d1d040865d05eb898c4374d3cd9453f2e4eb6fd3
김상민 <wrt123123123@gmail.com>
2021-03-09 13:29:32 +0900

    review

--- JavaEx/src/com/javaex/collection/hash/HashSetEx.java
+++ JavaEx/src/com/javaex/collection/hash/HashSetEx.java
@@ -1,15 +1,14 @@
 package com.javaex.collection.hash;
 
-
 import java.util.HashSet;
-import java.util.Iterator;
+
 public class HashSetEx {
+
 	public static void main(String[] args) {
-		basicHashSetEx();
-		//	customHashSetEx();
-		
-		
+//		basicHashSetEx();
+		customHashSetEx();
 	}
+
 	private static void customHashSetEx() {
 		HashSet<Student> hs = new HashSet<>();
 		
@@ -21,22 +20,21 @@ public class HashSetEx {
 		hs.add(s2);
 		hs.add(s3);
 		
-		System.out.println("학생부: " + hs);	//	요소들은 작동을 하지만 순서는 유지되지 않는다
+		System.out.println("학생부:" + hs);
 		
 		Student s4 = new Student("홍길동", 10);
 		hs.add(s4);
-		System.out.println("hashCode of s1: " + s1.hashCode());	//	메모리 주소가 출력된다 (object해쉬코드)
-		System.out.println("hashCode of s4: " + s4.hashCode());
-		System.out.println("s1 equals s4 ? " + s1.equals(s4));	//	두 해시코드의 메모리 주소가 같은지 확인할 수 있다
+		System.out.println("hashcode of s1:" + s1.hashCode());
+		System.out.println("hashcode of s4:" + s4.hashCode());
+		System.out.println("s1 equals s4 ? " + s1.equals(s4));
 		
-		System.out.println("학생부: " + hs);
+		System.out.println("학생부:" + hs);
 		
 		//	포함 여부 확인
 		boolean exists = hs.contains(new Student("홍길동", 10));
-		System.out.println("홍길동(10) exists: " + exists);
-		
-		
+		System.out.println("홍길동(10) exists:" + exists);
 	}
+	
 	private static void basicHashSetEx() {
 		HashSet<String> hs = new HashSet<>();
 		
@@ -46,33 +44,26 @@ public class HashSetEx {
 		hs.add("C++");
 		hs.add("Python");
 		
-		System.out.println("HashSet: " + hs);	//	자료는 정상 출력이 되지만 순서는 유지하지 않는다
-		System.out.println("요소의 수: " + hs.size());
+		System.out.println("HashSet:" + hs);	//	순서는 유지하지 않음
+		System.out.println("요소의 수:" + hs.size());
 		
 		hs.add("Java");
-		System.out.println("HashSet: " + hs);	//	hashset에서는 중복을 허용하지 않는다
+		System.out.println("HashSet:" + hs);	//	중복을 허용하지 않음
 		
 		//	요소의 포함 여부 확인
-		System.out.println("contauns Java ? " + hs.contains("Java"));
+		System.out.println("contains Java ? " + hs.contains("Java"));
 		System.out.println("contains Linux ? " + hs.contains("Linux"));
 		
-		System.out.println("HashSet: " + hs);
+		System.out.println("HashSet:" + hs);
 		
-		
-		//	반복자 활용
-		Iterator<String> it = map.keySet().iterator();
-		while (it.equals(hasNext()) {
-			ClassRoom item = map.get(it.net());
-			System.out.println(item);
-		}
 		//	요소의 삭제
 		hs.remove("C++");
-		System.out.println("HashSet: " + hs);
+		System.out.println("HashSet:" + hs);
 		
-		//	요소비우기
+		//	비우기
 		hs.clear();
-		System.out.println("HashSet: " + hs);
-		
+		System.out.println("HashSet:" + hs);
 		
 	}
+
 }
