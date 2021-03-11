8f8961122e06ef8f5e811f768038fe8c6a8f0c3d
김상민 <wrt123123123@gmail.com>
2021-03-11 10:36:39 +0900

    Server V1

--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -8,29 +8,30 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) {
-		//	서버 소켓 만들기
+		//	서버 소켓
 		ServerSocket serverSocket = null;
 		
 		try {
-			//	bind - IP연결
+			//	바인드 - IP 연결
 			serverSocket = new ServerSocket();
-			InetServerAddress ips = new InetSocketAddress("0.0.0.0", 10000);
+			InetSocketAddress ips = new InetSocketAddress("0.0.0.0", 10000);
 			serverSocket.bind(ips);
 			
-			//	시작메세지
+			//	시작 메시지
 			System.out.println("<서버 시작>");
-			System.out.println("SEVER: [연결을 기다립니다.]");
+			System.out.println("SERVER: [연결을 기다립니다.]");
 			
-			//	연결대기
+			//	연결 대기
 			Socket socket = serverSocket.accept();
-			//	접속이 허용이 되면 서버소켓으로 부터 클라이언트 정보를 받을 수 있다
+			//	클라이언트 정보 확인
 			InetSocketAddress socketAddress =
-					(InetSocketAddress)socket.getRemoteSocketAddress();	//	원격지 소켓의 주소
+					(InetSocketAddress)socket.getRemoteSocketAddress();	//	원격지 소켓의 주소 확인
 			System.out.println("SERVER: [클라이언트가 연결되었습니다]");
-			System.out.println("    클라이언트: " + socketAddress.getAddress() + ":" + socketAddress.getPort());
+			System.out.println("	클라이언트:" + socketAddress.getAddress() 
+								+ ":" + socketAddress.getPort());
 			
 			//	후처리
-			System.out.println("SEVER: [서버를 종료합니다.]");
+			System.out.println("SERVER: [서버를 종료합니다]");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -41,5 +42,4 @@ public class Server {
 			}
 		}
 	}
-	
 }
