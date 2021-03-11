1889aae83ad803efabe1eead962967445eac8c72
김상민 <wrt123123123@gmail.com>
2021-03-11 10:48:46 +0900

    Server and Client

--- JavaEx/src/com/javaex/network/echoserver/Client.java
+++ JavaEx/src/com/javaex/network/echoserver/Client.java
@@ -1,6 +1,10 @@
 package com.javaex.network.echoserver;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -16,17 +20,32 @@ public class Client {
 			
 			//	시작메세지
 			System.out.println("<클라이언트 시작>");
-			System.out.println("CLIENT: [연결을 요청합니다]");
+			System.out.println("CLIENT: [연결을 요청합니다.]");
 			
 			//	접속시도
 			InetSocketAddress remote = new InetSocketAddress("127.0.0.1", 10000);
 			socket.connect(remote);
 			
 			//	접속성공
-			System.out.println("CLIENT: [서버에 연결되었습니다]");
+			System.out.println("CLIENT: [서버에 연결되었습니다.]");
+			
+			
+			//	메세지 전송
+			OutputStream os = socket.getOutputStream();	//	소켓에서 출력 스트림 희득
+			Writer osw = new OutputStreamWriter(os, "UTF-8");
+			BufferedWriter bw = new BufferedWriter(osw);
+			
+			
+			String message = "테스트 메세지";
+			bw.write(message);
+			System.out.println("CLIENT: [전송메세지]: " + message);
+			
+			bw.close();
+			
+			
 			
 			//	후처리
-			System.out.println("<클라이언트가 종료되었습니다>");
+			System.out.println("<클라이언트가 종료되었습니다.>");
 		} catch (ConnectException e) {
 			System.err.println("[접속이 거부되었습니다]");
 		} catch (IOException e) {
--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -1,6 +1,10 @@
 package com.javaex.network.echoserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -26,12 +30,33 @@ public class Server {
 			//	클라이언트 정보 확인
 			InetSocketAddress socketAddress =
 					(InetSocketAddress)socket.getRemoteSocketAddress();	//	원격지 소켓의 주소 확인
-			System.out.println("SERVER: [클라이언트가 연결되었습니다]");
+			System.out.println("SERVER: [클라이언트가 연결되었습니다.]");
 			System.out.println("	클라이언트:" + socketAddress.getAddress() 
 								+ ":" + socketAddress.getPort());
 			
+			
+			//	메세지 수신
+			InputStream is = socket.getInputStream();
+			Reader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			//	메세지 읽어오기
+			String message;
+			
+			while(true) {
+				message = br.readLine();
+				
+				if (message == null) {	//	더 이상 읽어올 메세지가 없음
+					System.out.println("SERVER: [접속을 종료합니다.]");
+					break;
+				}
+				System.out.println("SERVER: [수신메세지]" + message);
+			}
+			
+			
+			
 			//	후처리
-			System.out.println("SERVER: [서버를 종료합니다]");
+			System.out.println("SERVER: [서버를 종료합니다.]");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
