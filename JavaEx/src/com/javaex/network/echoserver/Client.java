60a53b58ed4dcd5acaf87a5f400777af7b6f6849
김상민 <wrt123123123@gmail.com>
2021-03-11 11:23:39 +0900

    Server, client v3

--- JavaEx/src/com/javaex/network/echoserver/Client.java
+++ JavaEx/src/com/javaex/network/echoserver/Client.java
@@ -1,9 +1,13 @@
 package com.javaex.network.echoserver;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
@@ -18,34 +22,54 @@ public class Client {
 			//	소켓 객체 생성
 			socket = new Socket();
 			
-			//	시작메세지
+			//	시작 메시지
 			System.out.println("<클라이언트 시작>");
-			System.out.println("CLIENT: [연결을 요청합니다.]");
+			System.out.println("CLIENT: [연결을 요청합니다]");
 			
-			//	접속시도
+			//	접속 시도
 			InetSocketAddress remote = new InetSocketAddress("127.0.0.1", 10000);
 			socket.connect(remote);
 			
-			//	접속성공
-			System.out.println("CLIENT: [서버에 연결되었습니다.]");
+			//	접속 성공
+			System.out.println("CLIENT: [서버에 연결되었습니다]");
 			
 			
-			//	메세지 전송
-			OutputStream os = socket.getOutputStream();	//	소켓에서 출력 스트림 희득
+			
+			//	메시지 전송 
+			OutputStream os = socket.getOutputStream();	//	소켓에서 출력 스트림 획득
 			Writer osw = new OutputStreamWriter(os, "UTF-8");
 			BufferedWriter bw = new BufferedWriter(osw);
 			
+			//	Echo back 메시지 수신
+			InputStream is = socket.getInputStream();
+			Reader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
 			
-			String message = "테스트 메세지";
+			//	메시지 송신
+			String message = "테스트 메시지";
 			bw.write(message);
-			System.out.println("CLIENT: [전송메세지]: " + message);
+			bw.newLine();
+			bw.flush();
 			
+			System.out.println("CLIENT:[전송 메시지]:" + message);
+			
+			//	메시지 수신
+			String rcvMsg = br.readLine();
+			System.out.println("CLIENT:[수신 메시지]:" + rcvMsg);
+			
+			br.close();
 			bw.close();
 			
 			
 			
+			
+			
+			
+			
+			
 			//	후처리
-			System.out.println("<클라이언트가 종료되었습니다.>");
+			System.out.println("<클라이언트 종료>");
+			
 		} catch (ConnectException e) {
 			System.err.println("[접속이 거부되었습니다]");
 		} catch (IOException e) {
--- JavaEx/src/com/javaex/network/echoserver/Server.java
+++ JavaEx/src/com/javaex/network/echoserver/Server.java
@@ -1,10 +1,14 @@
 package com.javaex.network.echoserver;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -30,33 +34,48 @@ public class Server {
 			//	클라이언트 정보 확인
 			InetSocketAddress socketAddress =
 					(InetSocketAddress)socket.getRemoteSocketAddress();	//	원격지 소켓의 주소 확인
-			System.out.println("SERVER: [클라이언트가 연결되었습니다.]");
+			System.out.println("SERVER: [클라이언트가 연결되었습니다]");
 			System.out.println("	클라이언트:" + socketAddress.getAddress() 
 								+ ":" + socketAddress.getPort());
 			
 			
-			//	메세지 수신
+			
+			//	메시지 수신
 			InputStream is = socket.getInputStream();
 			Reader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
 			
-			//	메세지 읽어오기
+			//	Echo Back을 위한 OutputStream
+			OutputStream os = socket.getOutputStream();
+			Writer osw = new OutputStreamWriter(os, "UTF-8");
+			BufferedWriter bw = new BufferedWriter(osw);
+			
+			//	메시지 읽어오기
 			String message;
 			
 			while(true) {
 				message = br.readLine();
 				
-				if (message == null) {	//	더 이상 읽어올 메세지가 없음
-					System.out.println("SERVER: [접속을 종료합니다.]");
+				if (message == null) {	//	읽을 메시지 없음
+					System.out.println("SERVER: 접속을 종료합니다.");
 					break;
 				}
-				System.out.println("SERVER: [수신메세지]" + message);
+				
+				System.out.println("SERVER: [수신 메시지]" + message);
+				
+				//	Echo Back 메시지 전송
+				message = "Echo back - " + message;
+				System.out.println("SERVER: [Echo back] :" + message);
+				bw.write(message);
+				bw.newLine();
+				bw.flush();
 			}
 			
-			
-			
+			bw.close();
+			br.close();
+
 			//	후처리
-			System.out.println("SERVER: [서버를 종료합니다.]");
+			System.out.println("SERVER: [서버를 종료합니다]");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
